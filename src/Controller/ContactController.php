6a6597ebe724fac0e0fb0266ba14653022d415de
Hamza Ben Attia <hamzabenattia@isamm.u-manouba.tn>
2024-04-18 17:46:01 +0200

    .

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -71,7 +71,7 @@ class ContactController extends AbstractController
             ];
 
             $this->emailSender->sendEmail($data['email'],
-            'houcine.z@sonaso.fr',  
+            'hamzabenattiayt2@gmail.com',  
             'Nouveau message de contact Nouvsys',
             'emails/contact.html.twig',
             $context); 
